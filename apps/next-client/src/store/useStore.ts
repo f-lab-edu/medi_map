ca71bb8d2704d055dda1cb31a12cea2e4c3e6e22
henny <kimhyein1105@gmail.com>
2024-09-24 13:48:56 +0900

    chore: 프로젝트 초기 셋팅

---
+++ apps/next-client/src/store/useStore.ts
@@ -0,0 +1,8 @@
+import { create } from 'zustand';
+
+const useStore = create((set) => ({
+  message: '하하하하하하',
+  setMessage: (newMessage: string) => set({ message: newMessage })
+}));
+
+export default useStore;
