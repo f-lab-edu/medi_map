22a2fc33eb1cb292f8e277f4251856da6876ffa0
henny <kimhyein1105@gmail.com>
2024-09-24 16:22:32 +0900

    refactor: zustand api 호출 테스트

--- apps/next-client/src/store/useStore.ts
+++ apps/next-client/src/store/useStore.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 
-<<<<<<< HEAD
 interface PokemonState {
   pokemon: string[];
   fetchPokemon: () => Promise<void>;
@@ -18,16 +17,6 @@ const useStore = create<PokemonState>((set) => ({
     const data = await response.json();
     set({ pokemon: data.results.map((pokemon: PokemonResult) => pokemon.name) });
   },
-=======
-interface StoreState {
-  message: string;
-  setMessage: (msg: string) => void;
-}
-
-const useStore = create<StoreState>((set) => ({
-  message: 'TEST',  
-  setMessage: (newMessage: string) => set({ message: newMessage }),
->>>>>>> 23c8e14 (reractor: 타입스크립트 인터페이스 타입 추가)
 }));
 
 export default useStore;
