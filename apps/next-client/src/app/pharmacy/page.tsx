5b1d7b94e23abe3d6d04d74f62e4d67655b1cde8
henny <kimhyein1105@gmail.com>
2024-10-01 21:28:22 +0900

    fix: 충돌 오류 해결

--- apps/next-client/src/app/pharmacy/page.tsx
+++ apps/next-client/src/app/pharmacy/page.tsx
@@ -1,15 +1,6 @@
 export default function PharmacyPage() {
   return (
     <div>
-      <h1>로그인</h1>
-      <form />
-    </div>
-  );
-}
-
-export default function PharmacyPage() {
-  return (
-    <div>
       <h1>약국 찾기</h1>
     </div>
   );
