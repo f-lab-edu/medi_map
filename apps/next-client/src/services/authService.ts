19f70f9716ab426ff7ea4517095f5a77d00e5700
henny <kimhyein1105@gmail.com>
2024-10-10 17:44:09 +0900

    refactor: 로그인, 회원가입 hook 분리

--- apps/next-client/src/services/authService.ts
+++ apps/next-client/src/services/authService.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { signIn } from 'next-auth/react';
-import { ROUTES } from '@/constants/urls';
+import { ROUTES, API_URLS } from '@/constants/urls';
+import { ERROR_MESSAGES } from '@/constants/errors';
 
 export const loginWithCredentials = async (email: string, password: string) => {
   const result = await signIn('credentials', {
@@ -12,4 +14,18 @@ export const loginWithCredentials = async (email: string, password: string) => {
 
 export const loginWithGoogle = async () => {
   await signIn('google', { callbackUrl: ROUTES.HOME });
+};
+
+export const signup = async ({ username, email, password }) => {
+  try {
+    const response = await axios.post(API_URLS.SIGNUP, {
+      username,
+      email,
+      password,
+    });
+
+    return response;
+  } catch (error: unknown) {
+    throw new Error(error.response?.data?.message || ERROR_MESSAGES.SIGN_UP_ERROR);
+  }
 };
