8c8c45238d974e850488cded1f9bb84082fa16e9
henny <kimhyein1105@gmail.com>
2024-10-15 22:58:16 +0900

    fix: 비밀번호 해싱 중복 삭제

--- apps/express-server/src/services/authService.ts
+++ apps/express-server/src/services/authService.ts
@@ -1,17 +1,13 @@
 import pool from '@/db';
-import bcrypt from 'bcrypt';
 
-// 데이터베이스에서 사용자 생성
 export const createUser = async (username: string, email: string, password: string) => {
-  const hashedPassword = await bcrypt.hash(password, 10);
   const result = await pool.query(
     'INSERT INTO "Users" (username, email, password, "createdAt", "updatedAt") VALUES ($1, $2, $3, NOW(), NOW()) RETURNING *',
-    [username, email, hashedPassword]
+    [username, email, password]
   );
   return result.rows[0];
 };
 
-// 이메일을 통해 사용자 조회
 export const findUserByEmail = async (email: string) => {
   const result = await pool.query('SELECT * FROM "Users" WHERE email = $1', [email]);
   return result.rows[0];
