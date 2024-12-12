e426d605ed659dc261fa49299a1d305f625b0283
henny <kimhyein1105@gmail.com>
2024-12-12 21:01:31 +0900

    chore: 필요 없는 주석 삭제

--- apps/next-client/src/services/loginService.ts
+++ apps/next-client/src/services/loginService.ts
@@ -19,7 +19,6 @@ export const loginWithCredentials = async (email: string, password: string) => {
       throw new LoginError(result.error || ERROR_MESSAGES.LOGIN_ERROR);
     }
 
-    // 여기서는 바로 accessToken을 받지 않고 result만 반환
     return result;
   } catch (error: unknown) {
     handleLoginError(error);
@@ -28,7 +27,6 @@ export const loginWithCredentials = async (email: string, password: string) => {
 
 export const loginWithGoogle = async () => {
   try {
-    // [수정 포인트] redirect를 false로 설정
     const result = await signIn('google', { redirect: false, callbackUrl: ROUTES.HOME });
 
     if (!result) {
@@ -39,7 +37,6 @@ export const loginWithGoogle = async () => {
       throw new LoginError(ERROR_MESSAGES.GOOGLE_LOGIN_ERROR);
     }
 
-    // 구글 로그인도 여기서 fetch하지 않고 result만 반환
     return result;
   } catch (error: unknown) {
     handleLoginError(error);
