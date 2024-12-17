da9c4719c69a9e1fece422efcfb4b5efa89c280f
henny <kimhyein1105@gmail.com>
2024-12-17 21:11:28 +0900

    chore: 불필요 주석 삭제

--- apps/express-server/src/migrations/20241216051404-mypage.js
+++ apps/express-server/src/migrations/20241216051404-mypage.js
@@ -3,14 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // provider 컬럼 추가
     await queryInterface.addColumn('Users', 'provider', {
       type: Sequelize.STRING,
       allowNull: false,
-      defaultValue: 'credentials', // 기본값 설정
+      defaultValue: 'credentials',
     });
 
-    // providerId 컬럼 추가
     await queryInterface.addColumn('Users', 'providerId', {
       type: Sequelize.STRING,
       allowNull: true,
@@ -18,10 +16,7 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    // rollback: provider 컬럼 제거
     await queryInterface.removeColumn('Users', 'provider');
-
-    // rollback: providerId 컬럼 제거
     await queryInterface.removeColumn('Users', 'providerId');
   },
 };
