5bbc83e32296b67673ec1dde8d7a3d5634967105
henny <kimhyein1105@gmail.com>
2025-01-29 02:36:57 +0900

    chore: 환경변수 타입 선언 삭제

--- apps/next-client/src/services/axiosInstance.ts
+++ apps/next-client/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_LOCAL_BACKEND_URL as string,
+  baseURL: process.env.NEXT_PUBLIC_LOCAL_BACKEND_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
