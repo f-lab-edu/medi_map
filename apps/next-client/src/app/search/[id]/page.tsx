df83f8c877811abdc40c70fa7c1f1ebe6ef20ac4
henny <kimhyein1105@gmail.com>
2025-03-10 17:24:38 +0900

    refactor: ErrorBoundary 기능 추가

--- apps/next-client/src/app/search/[id]/page.tsx
+++ apps/next-client/src/app/search/[id]/page.tsx
@@ -3,14 +3,17 @@
 import React, { Suspense } from 'react';
 import '@/styles/pages/search/search.scss';
 import MedicineDetailView from '@/components/medicine/MedicineDetailView';
+import ErrorBoundary from '@/components/common/ErrorBoundary';
 
 export default function MedicineDetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const medicineId = Array.isArray(id) ? id[0] : id;
 
   return (
-    <Suspense fallback={<p>로딩 중...</p>}>
-      <MedicineDetailView medicineId={medicineId} />
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<p>로딩 중...</p>}>
+        <MedicineDetailView medicineId={medicineId} />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
