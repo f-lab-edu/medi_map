9dc0bc07ff04e40f4fb4c811fb5ce06237ae45c1
henny <kimhyein1105@gmail.com>
2025-03-10 23:16:13 +0900

    chore: fetchPost axios 에러 추가

--- apps/next-client/src/app/community/[id]/page.tsx
+++ apps/next-client/src/app/community/[id]/page.tsx
@@ -3,22 +3,32 @@ import PostContent from '@/components/community/PostContent';
 import PostDetailPage from '@/components/community/PostDetailPage';
 import '@/styles/pages/community/community.scss';
 import { ERROR_MESSAGES } from '@/constants/errors';
+import { AxiosError } from 'axios';
+import { axiosInstance } from '@/services/axiosInstance';
 
 async function fetchPost(id: string) {
-  const response = await fetch(`${API_URLS.POSTS}/${id}`, { cache: 'no-store' });
-  
-  if (!response.ok) {
-    let errorMessage = `게시글을 불러올 수 없습니다. 상태 코드: ${response.status}`;
+  try {
+    const response = await axiosInstance.get(`${API_URLS.POSTS}/${id}`, {
+      headers: { 'Cache-Control': 'no-store' },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      let errorMessage: string = ERROR_MESSAGES.CLIENT_ERROR;
 
-    if (response.status === 404) {
-      errorMessage = ERROR_MESSAGES.POST_NOT_FOUND;
-    } else if (response.status === 500) {
-      errorMessage = ERROR_MESSAGES.SERVER_ERROR;
+      if (error.response?.status === 404) {
+        errorMessage = ERROR_MESSAGES.POST_NOT_FOUND;
+      } else if (error.response?.status === 500) {
+        errorMessage = ERROR_MESSAGES.SERVER_ERROR;
+      }
+
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      console.error("Failed to fetch post:", error);
+      throw new Error(ERROR_MESSAGES.UNKNOWN_ERROR);
     }
-    console.error(errorMessage);
-    throw new Error(errorMessage);
   }
-  return response.json();
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
